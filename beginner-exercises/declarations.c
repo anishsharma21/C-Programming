61614e0bb41f78aca786d29020cfb83e42713d9e
anishsharma21 <anisharma2103@gmail.com>
2024-10-10 21:48:49 +1100

    Beginner C exercises - simple program that takes in user input and prints it out. My strategy from here on in is to instead complete mini projects rather than learn each part of the syntax like I am doing right now - better to apply my knowledge and learn through building

---
+++ beginner-exercises/declarations.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    float b;
+    char c;
+
+    a = 42;
+    b = 3.14159;
+    c = 'A';
+
+    printf("%d\n%.2f\n%c\n", a, b, c);
+    
+    return 0;
+}
