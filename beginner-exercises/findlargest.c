22380df5ea341f051fc635deaee4af34f16cb02b
anishsharma21 <anisharma2103@gmail.com>
2024-10-11 14:12:38 +1100

    Beginner C exercises - finding largest of 3 numbers, using pointers too.

---
+++ beginner-exercises/findlargest.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int first, second, third;
+    printf("First number: ");
+    scanf("%d", &first);
+    printf("Second number: ");
+    scanf("%d", &second);
+    printf("Third number: ");
+    scanf("%d", &third);
+
+    int *largest = NULL;
+    if (first > second) {
+	if (first > third) {
+	    largest = &first;
+	} else {
+	    largest = &third;
+	}
+    } else {
+	if (second > third) {
+	    largest = &second;
+	} else {
+	    largest = &third;
+	}
+    }
+
+    printf("Largest value: %d\n", *largest);
+    return 0;
+}
