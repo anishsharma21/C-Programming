84629e3b98752dc1e215e7784a2758aafb3c9e88
anishsharma21 <anisharma2103@gmail.com>
2024-10-11 11:36:21 +1100

    Beginner C exercises - simple interest calculator.

---
+++ beginner-exercises/interestcalc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float calcsimpint(float principal, float rate, float time);
+void calcsimpintp(float *principal, float *rate, float *time, float *value);
+
+int main() {
+    float principal, rate, time, value;
+    printf("Principal: ");
+    scanf("%f", &principal);
+    printf("Rate: ");
+    scanf("%f", &rate);
+    printf("Time: ");
+    scanf("%f", &time);
+    float result = calcsimpint(principal, rate, time);
+    printf("%.f\n", result);
+    calcsimpintp(&principal, &rate, &time, &value);
+    printf("%.f\n", value);
+}
+
+float calcsimpint(float principal, float rate, float time) {
+    return principal * (rate / 100) * time;
+}
+
+void calcsimpintp(float *principal, float *rate, float *time, float *value) {
+    *value = *principal * (*rate / 100) * *time;
+}
