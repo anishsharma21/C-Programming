3c976776808e18f88d55076eec8adbe54e61ed78
anishsharma21 <anisharma2103@gmail.com>
2024-10-13 17:57:42 +1100

    Beginner C exercises - simple wc utility program.

---
+++ beginner-exercises/wc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main() {
+    int c = 0, wc = 0;
+    int status = OUT;
+    while ((c = getchar()) != EOF) {
+	if (c == '\n' || c == '\t' || c == ' ') {
+	    status = OUT;
+	} else {
+	    if (status == OUT) {
+		wc++;
+		status = IN;
+	    }
+	}
+    }
+    printf("\nWord count: %d", wc);
+    return 0;
+}
