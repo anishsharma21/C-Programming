c694352b75aafbd7da4ad7868c6d2d772771dbac
anishsharma21 <anisharma2103@gmail.com>
2024-10-13 17:36:52 +1100

    Beginner C exercises - prime number program.

---
+++ beginner-exercises/primenum.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool prime(int num);
+
+int main() {
+    int num;
+    bool isPrime = 0;
+    printf("Which number to check: ");
+    scanf("%d", &num);
+    if (num > 1) {
+	isPrime = prime(num);
+    }
+    if (isPrime) {
+	printf("%d is prime\n", num);
+    } else {
+	printf("%d is not prime\n", num);
+    }
+    return 0;
+}
+
+bool prime(int num) {
+    for (int i = 2; i < num; i++) {
+	if (i * i > num)
+	    return true;
+	if (num % i == 0)
+	    return false;
+    }
+    return true;
+}
