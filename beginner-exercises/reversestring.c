9bcf4787e16a5f83e9a4469b1305666bcc42fdc2
anishsharma21 <anisharma2103@gmail.com>
2024-10-15 20:36:17 +1100

    Beginner C exercises - reverse a string.

---
+++ beginner-exercises/reversestring.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void reverseString(char string[], int length);
+
+int main() {
+    char string[] = "bottle";
+    int stringLength = sizeof(string) / sizeof(char);
+    printf("Initial string: %s\n", string);
+    reverseString(string, stringLength);
+    printf("Reversed string: %s\n", string);
+    return 0;
+}
+
+void reverseString(char string[], int length) {
+    char temp;
+    for (int i = 0; i < (length - 1) / 2; i++) {
+        temp = string[i];
+        string[i] = string[length - i - 2];
+        string[length - i - 2] = temp;
+    }
+}
