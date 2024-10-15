887ff46a314593c7e54f804c9bf30bdd663d8234
anishsharma21 <anisharma2103@gmail.com>
2024-10-15 11:07:48 +1100

    Beginner C exercises - sum of digits program.

---
+++ beginner-exercises/sumofdigits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int findsumofdigits(int num);
+
+int main() {
+    int input = 0;
+    printf("Enter a number: ");
+    scanf("%d", &input);
+
+    int dsum = findsumofdigits(input);
+    printf("Sum of digits: %d\n", dsum);
+
+    return 0;
+}
+
+int findsumofdigits(int num) {
+    int sum = 0;
+    for (int i = 1; i - num < 0; i *= 10) {
+        int rem = num % (i * 10);
+        num -= rem;
+        sum += rem / i;
+    }
+    return sum;
+}
