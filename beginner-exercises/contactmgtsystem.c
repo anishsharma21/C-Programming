4951a0ef02316c759a6976fa36576ef3cd446f57
anishsharma21 <anisharma2103@gmail.com>
2024-10-27 17:07:29 +1100

    Beginner C exercises - contact management system. Used malloc and pointers to allocate space and then set values on the Contact struct.

--- beginner-exercises/contactmgtsystem.c
+++ beginner-exercises/contactmgtsystem.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     char name[16];
@@ -9,8 +10,18 @@ typedef struct {
 
 int main() {
     Contact c1 = {"Anish", 21, "044444444", "anish@gmail.com"};
-    printf("Name: %s, Age: %d\n", c1.name, c1.age);
-    printf("Size of Contact object: %lu\n", sizeof(Contact));
-    printf("Size of c1 Contact object: %lu\n", sizeof(c1));
+    printf("Name: %s, Age: %d, Phone Number: %s, Email: %s\n", c1.name, c1.age, c1.phoneNumber, c1.email);
+
+    Contact* c2 = (Contact*)malloc(sizeof(Contact));
+    if (c2 == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return 1;
+    }
+    snprintf(c2->name, sizeof(c2->name), "Kai");
+    c2->age = 22;
+    snprintf(c2->phoneNumber, sizeof(c2->phoneNumber), "0144444444");
+    snprintf(c2->email, sizeof(c2->email), "kai@gmail.com");
+    printf("Name: %s, Age: %d, Phone Number: %s, Email: %s\n", c2->name, c2->age, c2->phoneNumber, c2->email);
+
     return 0;
 }
