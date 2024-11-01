b4970b9557f2e77c5223e1cbcd42a328b2d12683
anishsharma21 <anisharma2103@gmail.com>
2024-11-02 01:02:27 +1100

    Beginner C exercises - contact management system. Add contact functionality added to append new contact to the file. Forgot to fclose files, ran into some issues along the way, figured it out eventually - lots of ways to do the same thing in C, it's a little confusing at times.

--- beginner-exercises/contactmgtsystem.c
+++ beginner-exercises/contactmgtsystem.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define NUMBER_OF_CONTACT_ATTRIBUTES 4
 
 void viewContacts();
+void addContact();
 void clearInputBuffer();
 
 typedef struct {
@@ -32,11 +35,13 @@ int main() {
                 break;
             case 'a':
                 printf("User choice is to add a new contact\n");
+                addContact();
                 break;
             case 'd':
                 printf("User choice is to delete a contact\n");
                 break;
             case 'q':
+                system("clear");
                 return 0;
             default:
                 printf("Choose a valid option (v, a, d, q)\n");
@@ -49,13 +54,13 @@ int main() {
 
 // we want to display first name, last name, age, and email
 void viewContacts() {
+    system("clear");
     FILE* pF;
     pF = fopen("contacts.txt", "r");
     if (pF == NULL) {
         fprintf(stderr, "Error opening contacts file.\n");
         return;
     }
-
     printf("Contacts:\n");
     char buffer[100];
     while (fgets(buffer, sizeof(buffer), pF) != NULL) {
@@ -70,6 +75,33 @@ void viewContacts() {
         }
         printf("\n");
     }
+    fclose(pF);
+}
+
+void addContact() {
+    system("clear");
+    FILE* pF;
+    pF = fopen("contacts.txt", "a");
+    if (pF == NULL) {
+        fprintf(stderr, "Error opening contacts file.\n");
+        return;
+    }
+
+    char buffer[50];
+    char prompts[][16] = {"First name: ", "Last name: ", "Age: ", "Email: "};
+    for (int i = 0; i < NUMBER_OF_CONTACT_ATTRIBUTES; i++) {
+        printf("%s", prompts[i]);
+        if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+            buffer[strcspn(buffer, "\n")] = '\0';
+            fprintf(pF, "%s", buffer);
+            if (i + 1 != NUMBER_OF_CONTACT_ATTRIBUTES)
+                fprintf(pF, ",");
+        }
+    }
+    fprintf(pF, "\n");
+    fclose(pF);
+
+    printf("\nAdded contact successfully!\n");
 }
 
 void clearInputBuffer() {
