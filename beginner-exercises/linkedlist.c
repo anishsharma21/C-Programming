183f372121af5a1a655ff722ca35580cb94ab612
anishsharma21 <anisharma2103@gmail.com>
2024-11-03 23:03:35 +1100

    Beginner C exercises - linked list. Node creation, linked list creation, displaying node values, initialising linked list and seeing pointer values.

--- beginner-exercises/linkedlist.c
+++ beginner-exercises/linkedlist.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct Node {
     int data;
     struct Node* next;
 } Node;
 
+typedef struct LinkedList {
+    Node* head;
+    Node* tail;
+    int length;
+} LinkedList;
+
+Node* createNode();
+void displayNodeValue(Node* node);
+LinkedList* createLinkedList();
+
 int main() {
-    Node* node1 = (Node*)malloc(sizeof(Node));
-    node1->data = 3;
-    printf("Node value: %d\n", node1->data);
+    LinkedList* linkedList = createLinkedList();
+    printf("Length of linked list: %d\n", linkedList->length);
+    printf("Address of head node: %p\nAddress of tail node: %p\n", linkedList->head, linkedList->tail);
+    printf("Value of head node: %d\nValue of tail node: %d\n", linkedList->head->data, linkedList->tail->data);
     return 0;
+}
+
+Node* createNode() {
+    return (Node*)malloc(sizeof(Node));
+}
+
+void displayNodeValue(Node* node) {
+    printf("Node value: %d\n", node->data);
+}
+
+LinkedList* createLinkedList() {
+    Node* node = createNode();
+    LinkedList* linkedList = (LinkedList*)malloc(sizeof(LinkedList));
+    linkedList->head = node;
+    linkedList->tail = node;
+    return linkedList;
 }
