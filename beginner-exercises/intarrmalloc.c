e84ac71f9a437d81e4839bccba9eae699e98a67d
anishsharma21 <anisharma2103@gmail.com>
2024-11-04 14:13:07 +1100

    Beginner C exercises - created file for shell program, realised I need more experience with a few concepts like memory management, process management, signals, etc - working on a intarrmalloc program to get more experience with dynamic memory allocation.

---
+++ beginner-exercises/intarrmalloc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* allocateInt(int value);
+int* allocateIntArr(int size);
+
+int main() {
+    int size = 1;
+
+    printf("Size of int array: ");
+    scanf("%d", &size);
+
+    int* intPointer = allocateInt(size);
+    printf("\nInteger value: %d\nInteger address: %p\n", *intPointer, intPointer);
+    printf("Address of the pointer: %p\n", &intPointer);
+
+    printf("\nInteger array: ");
+    int* intArrPointer = allocateIntArr(size);
+    for (int i = 0; i < size; i++) {
+        if (i == 0)
+            printf("%d", intArrPointer[i]);
+        else
+            printf(", %d", intArrPointer[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+int* allocateInt(int value) {
+    int* intPointer = (int*)malloc(sizeof(int));
+    *intPointer = value;
+    return intPointer;
+}
+
+int* allocateIntArr(int size) {
+    int* intArrPointer = (int*)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
+        intArrPointer[i] = i + 1;
+    return intArrPointer;
+}
---
+++ mini-projects/shell.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("My shell");
+    return 0;
+}
