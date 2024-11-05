da1367377fabcae40e86aeb018d3a77cc01de300
anishsharma21 <anisharma2103@gmail.com>
2024-11-05 18:26:43 +1100

    C memory manager - function for initialising the memory pool, as opposed to a heap with sbrk, since mmap is being used and not sbrk which is deprecated. This way, I am asking the OS (which will talk to its virtual memory management system) to allocate memory space, or a memory pool in this instance, for the memory allocator. There are some flags that need to be set which achieve this. I will only need to use a singular free list even though in the past I have used multiple linked lists - they were not necessary at all, much simpler, efficient, and less overhead with just a singular free list which you can allocate from.

--- mini-projects/memory-manager.c
+++ mini-projects/memory-manager.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define POOL_SIZE 1024
 
 typedef struct Block {
     struct Block* next;
     struct Block* prev;
-    char name[32];
-    int value;
+    size_t size;
 } Block;
+
+void initialise_memory_pool();
+
+Block* free_list = NULL;
+void* memory_pool = NULL;
 
 int main() {
     return 0;
+}
+
+void initialise_memory_pool() {
+    memory_pool = mmap(NULL, POOL_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    if (memory_pool == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
 }
